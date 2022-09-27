8d8d1c907d3831455e3e433f4395d0698be8c0f8
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2022-09-27 23:28:25 +0600

    image link change

--- index.js
+++ index.js
@@ -9,37 +9,44 @@ app.use(cors());
 
 const locationRoute = require('./routes')
 app.set("views", path.join(__dirname, "views"));
+// app.use("/public/", express.static("./public"));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.render("form");
-// });
-// app.post("/", (req, res) => {
-//   const { year, day, hour, lat, long, magnitude } = {...req.body};
-//   const location_json = {
-//     year: year,
-//     day: day,
-//     h:hour,
-//     m:0,
-//     s:35,
-//     lat:lat,
-//     lon:long,
-//     magnitude:magnitude,
-//     comments:""
-//   };
-//   fs.writeFile("location.json", JSON.stringify(location_json), function (err) {
-//     if (err) throw err;
-//     console.log("Replaced!");
-//   });
-//   res.render("form", {
-//     // first: fname,
-//     // last: lname,
-//     // email: email,
-//     // birth: birthday,
-//   });
-// });
+app.get("/form", (req, res) => {
+  res.render("form");
+});
+
+app.get("/images",(req,res)=>{
+  res.render("image")
+})
+
+app.post("/form_data", (req, res) => {
+  const { year, day, hour, lat, long, magnitude } = {...req.body};
+  const location_json = {
+    year: year,
+    day: day,
+    h:hour,
+    m:0,
+    s:35,
+    lat:lat,
+    lon:long,
+    magnitude:magnitude,
+    comments:""
+  };
+  fs.writeFile("location.json", JSON.stringify(location_json), function (err) {
+    if (err) throw err;
+    console.log("Replaced!");
+  });
+  res.render("form", {
+    // first: fname,
+    // last: lname,
+    // email: email,
+    // birth: birthday,
+  });
+});
 
 app.use("/",locationRoute)
 const port = 3015;
